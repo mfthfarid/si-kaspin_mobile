9a3b5fbce8b0ca9c1ec56ea66751f5e59af4a2f4
arieeefajar <ariefajar739@gmail.com>
2024-06-08 21:40:22 +0700

    fix: menu penjualan

--- lib/services/ApiConfig.dart
+++ lib/services/ApiConfig.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String url = 'https://a94c-112-78-133-105.ngrok-free.app';
+  static const String url = 'https://9584-112-78-133-105.ngrok-free.app';
 
   static const String baseUrl = url + '/api';
 
